31117034a1857afbabd09cf7add0772f553a6c8b
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2026-01-02 23:44:01 +0100

    Fix history module not found

--- apps/web/app/(app)/history/page.tsx
+++ apps/web/app/(app)/history/page.tsx
@@ -1,32 +1,12 @@
 "use client";
-import { useState } from "react";
-import { Contract, HttpHandler, RpcClient } from "casper-js-sdk";
 import { BridgingCard } from "./_components/bridging-card";
-import Wallet from "./_components/wallet";
 
 export default function BridgingPage() {
-  // const [publicKey, setPublicKey] = useState(null);
-  // const CasperWalletProvider = window?.CasperWalletProvider;
-
-  // if (CasperWalletProvider == null) {
-  //   return <h1>Casper Wallet not Installed</h1>;
-  // }
-
-  // const provider = CasperWalletProvider();
-
-  // const rpcHandler = new HttpHandler("http://<Node Address>:7777/rpc");
-  // const rpcCient = new RpcClient(rpcHandler);
-
   return (
     <main
       className="container mx-auto p-4 overflow-y-auto"
       style={{ minHeight: "calc(100vh - 80px)" }}
     >
-      {/* <Wallet
-        publicKey={publicKey}
-        setPublicKey={setPublicKey}
-        provider={provider}
-      /> */}
       <BridgingCard />
     </main>
   );
