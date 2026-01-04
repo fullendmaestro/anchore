2353ba69f1b748f9453daf5ca49fe216b93c212a
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2026-01-04 01:19:19 +0100

    Fix swap quote

--- apps/web/app/(app)/bridge/_hooks/useSwapQuote.ts
+++ apps/web/app/(app)/bridge/_hooks/useSwapQuote.ts
@@ -2,9 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useRef } from "react";
 
-export type SwapQuote = ReturnType<typeof useSwapQuote>["swapQuote"];
-
-const useSwapQuote = (quoteRequest: QuoteRequest) => {
+const useSwapQuote = () => {
   return;
 };
 
